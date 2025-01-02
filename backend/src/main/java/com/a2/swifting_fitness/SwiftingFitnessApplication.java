b8800f88bb19cbe2a06427452928af5dfa0a49b7
Aradhya Nepal <aradhya.1221@gmail.com>
2025-01-02 19:38:45 +0545

    Initial Commit

---
+++ backend/src/main/java/com/a2/swifting_fitness/SwiftingFitnessApplication.java
@@ -0,0 +1,13 @@
+package com.a2.swifting_fitness;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwiftingFitnessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwiftingFitnessApplication.class, args);
+	}
+
+}
