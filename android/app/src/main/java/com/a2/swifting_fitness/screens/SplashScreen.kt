c6487a80b6bcf41233702633d7303b4a7c154d08
Aradhya Nepal <aradhya.1221@gmail.com>
2025-03-04 12:01:15 +0545

    Making splash screen

--- android/app/src/main/java/com/a2/swifting_fitness/screens/SplashScreen.kt
+++ android/app/src/main/java/com/a2/swifting_fitness/screens/SplashScreen.kt
@@ -1,7 +1,70 @@
 package com.a2.swifting_fitness.screens
 
+import android.annotation.SuppressLint
+import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.animation.core.LinearEasing
+import androidx.compose.animation.core.RepeatMode
+import androidx.compose.animation.core.animateFloat
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.infiniteRepeatable
+import androidx.compose.animation.core.rememberInfiniteTransition
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.rotate
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.a2.swifting_fitness.R
 
-class SplashScreen(goToLoginScreen: () -> Unit,goToOnBoardingScreen: () -> Unit) :ComponentActivity() {
+@SuppressLint("CustomSplashScreen")
+class SplashScreen(goToLoginScreen: () -> Unit, goToOnBoardingScreen: () -> Unit) :
+    ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            SplashContent()
+        }
+    }
+}
 
+@Preview(showSystemUi = true)
+@Composable
+private fun Preview() {
+    SplashContent()
+}
+
+@Composable
+fun SplashContent(modifier: Modifier = Modifier) {
+    val infiniteTransition = rememberInfiniteTransition(label = "rotation")
+    val angle = infiniteTransition.animateFloat(
+        initialValue = 0f,
+        targetValue = 360f,
+        animationSpec = infiniteRepeatable(
+            animation = tween(durationMillis = 2000, easing = LinearEasing),
+            repeatMode = RepeatMode.Restart
+        ),
+        label = "angle"
+    )
+
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = modifier
+            .background(Color(0xFFFF0000))
+            .fillMaxSize()
+    ) {
+        Image(
+            painter = painterResource(R.drawable.app_logo),
+            contentDescription = "Logo",
+            modifier=Modifier.rotate(angle.value)
+
+            )
+    }
 }
