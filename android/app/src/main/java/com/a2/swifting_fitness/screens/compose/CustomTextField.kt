a535cfc48f01e807a8d09a1c49e075e86626536b
Aradhya Nepal <aradhya.1221@gmail.com>
2025-03-07 14:49:34 +0545

    Adding validation

--- android/app/src/main/java/com/a2/swifting_fitness/screens/compose/CustomTextField.kt
+++ android/app/src/main/java/com/a2/swifting_fitness/screens/compose/CustomTextField.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.size
@@ -41,100 +42,111 @@ fun CustomTextField(
     multiLine: Boolean = false,
     keyboardType: KeyboardType = KeyboardType.Text,
     action: ImeAction = ImeAction.Next,
+    showError:Boolean,
+    errorIfInvalid:String?=null,
     isPassword: Boolean = false,
     trailingIcon: @Composable (() -> Unit)? = null,
     leadingIcon: @Composable (() -> Unit)
 ) {
 
     val (passwordVisible, setPasswordVisible) = remember { mutableStateOf(isPassword) }
-    Box(
-        modifier = modifier
-            .fillMaxWidth()
-            .background(color = Color(0x11FFFFFF), shape = RoundedCornerShape(12.dp)),
-    ) {
-        OutlinedTextField(
+    Column {
+        Box(
             modifier = modifier
-                .fillMaxWidth(),
-            label = { Text(label, color = Color.White) },
-            value = value,
-            leadingIcon = leadingIcon,
-            trailingIcon = {
-                trailingIcon?.invoke()
-                if (isPassword) {
-                    val image =
-                        if (passwordVisible) R.drawable.solid_eye else R.drawable.app_logo
-                    val description = if (passwordVisible) "Hide password" else "Show password"
-                    Image(
-                        contentDescription = description,
-                        painter = painterResource(image),
-                        modifier = Modifier.clickable {
-                            setPasswordVisible(!passwordVisible)
-                        }.size(30.dp)
+                .fillMaxWidth()
+                .background(color = Color(0x11FFFFFF), shape = RoundedCornerShape(12.dp)),
+        ) {
+            OutlinedTextField(
+                isError = errorIfInvalid != null && showError,
+                modifier = modifier
+                    .fillMaxWidth(),
+                label = { Text(label, color = Color.White) },
+                value = value,
+                leadingIcon = leadingIcon,
+                trailingIcon = {
+                    trailingIcon?.invoke()
+                    if (isPassword) {
+                        val image =
+                            if (passwordVisible) R.drawable.solid_eye else R.drawable.app_logo
+                        val description = if (passwordVisible) "Hide password" else "Show password"
+                        Image(
+                            contentDescription = description,
+                            painter = painterResource(image),
+                            modifier = Modifier
+                                .clickable {
+                                    setPasswordVisible(!passwordVisible)
+                                }
+                                .size(30.dp)
 
-                    )
+                        )
 
-                }
-            },
-            onValueChange = { textFieldValue -> onValueChange(textFieldValue) },
-            visualTransformation = if (passwordVisible) PasswordVisualTransformation() else VisualTransformation.None,
-            keyboardOptions = KeyboardOptions.Default.copy(
-                keyboardType = keyboardType,
-                imeAction = action
-            ),
-            textStyle = TextStyle(color = Color.White, fontWeight = FontWeight.Bold),
-            singleLine = !multiLine,
-            maxLines = if (multiLine) 5 else 1,
-            readOnly = false,
-            enabled = enabled,
-
-            colors = TextFieldDefaults.colors(
-                focusedTextColor = Color.Blue,
-                unfocusedTextColor = Color.Transparent,
-                disabledTextColor = Color.Transparent,
-                errorTextColor = Color.Red,
-                focusedContainerColor = Color.Transparent,
-                unfocusedContainerColor = Color.Transparent,
-                disabledContainerColor = Color.Transparent,
-                errorContainerColor = Color.Red,
-                cursorColor = Color.Blue,
-                errorCursorColor = Color.Red,
-                selectionColors = TextSelectionColors(
-                    handleColor = Color.Blue,
-                    backgroundColor = Color.Transparent.copy(alpha = 0.1f)
+                    }
+                },
+                onValueChange = { textFieldValue -> onValueChange(textFieldValue) },
+                visualTransformation = if (passwordVisible) PasswordVisualTransformation() else VisualTransformation.None,
+                keyboardOptions = KeyboardOptions.Default.copy(
+                    keyboardType = keyboardType,
+                    imeAction = action
                 ),
-                focusedIndicatorColor = Color.Gray,
-                unfocusedIndicatorColor = Color.Transparent,
-                disabledIndicatorColor = Color.Transparent,
-                errorIndicatorColor = Color.Yellow,
-                focusedLeadingIconColor = Color.White,
-                unfocusedLeadingIconColor = Color.White,
-                disabledLeadingIconColor = Color.White,
-                errorLeadingIconColor = Color.White,
-                focusedTrailingIconColor = Color.Magenta,
-                unfocusedTrailingIconColor = Color.Cyan,
-                disabledTrailingIconColor = Color.Transparent,
-                errorTrailingIconColor = Color.Red,
-                focusedLabelColor = Color.Magenta,
-                unfocusedLabelColor = Color.Transparent,
-                disabledLabelColor = Color.Transparent,
-                errorLabelColor = Color.Red,
-                focusedPlaceholderColor = Color.Magenta,
-                unfocusedPlaceholderColor = Color.Transparent,
-                disabledPlaceholderColor = Color.Transparent,
-                errorPlaceholderColor = Color.Red,
-                focusedSupportingTextColor = Color.Gray,
-                unfocusedSupportingTextColor = Color.Transparent,
-                disabledSupportingTextColor = Color.Transparent,
-                errorSupportingTextColor = Color.Red,
-                focusedPrefixColor = Color.Blue,
-                unfocusedPrefixColor = Color.Transparent,
-                disabledPrefixColor = Color.Transparent,
-                errorPrefixColor = Color.Red,
-                focusedSuffixColor = Color.Blue,
-                unfocusedSuffixColor = Color.Transparent,
-                disabledSuffixColor = Color.Transparent,
-                errorSuffixColor = Color.Red
+                textStyle = TextStyle(color = Color.White, fontWeight = FontWeight.Bold),
+                singleLine = !multiLine,
+                maxLines = if (multiLine) 5 else 1,
+                readOnly = false,
+                enabled = enabled,
+
+                colors = TextFieldDefaults.colors(
+                    focusedTextColor = Color.Blue,
+                    unfocusedTextColor = Color.Transparent,
+                    disabledTextColor = Color.Transparent,
+                    errorTextColor = Color.Red,
+                    focusedContainerColor = Color.Transparent,
+                    unfocusedContainerColor = Color.Transparent,
+                    disabledContainerColor = Color.Transparent,
+                    errorContainerColor = Color.Transparent,
+                    cursorColor = Color.Blue,
+                    errorCursorColor = Color.Red,
+                    selectionColors = TextSelectionColors(
+                        handleColor = Color.Blue,
+                        backgroundColor = Color.Transparent.copy(alpha = 0.1f)
+                    ),
+                    focusedIndicatorColor = Color.Gray,
+                    unfocusedIndicatorColor = Color.Transparent,
+                    disabledIndicatorColor = Color.Transparent,
+                    errorIndicatorColor = Color.Transparent,
+                    focusedLeadingIconColor = Color.White,
+                    unfocusedLeadingIconColor = Color.White,
+                    disabledLeadingIconColor = Color.White,
+                    errorLeadingIconColor = Color.White,
+                    focusedTrailingIconColor = Color.Magenta,
+                    unfocusedTrailingIconColor = Color.Cyan,
+                    disabledTrailingIconColor = Color.Transparent,
+                    errorTrailingIconColor = Color.Red,
+                    focusedLabelColor = Color.Magenta,
+                    unfocusedLabelColor = Color.Transparent,
+                    disabledLabelColor = Color.Transparent,
+                    errorLabelColor = Color.Red,
+                    focusedPlaceholderColor = Color.Magenta,
+                    unfocusedPlaceholderColor = Color.Transparent,
+                    disabledPlaceholderColor = Color.Transparent,
+                    errorPlaceholderColor = Color.Red,
+                    focusedSupportingTextColor = Color.Gray,
+                    unfocusedSupportingTextColor = Color.Transparent,
+                    disabledSupportingTextColor = Color.Transparent,
+                    errorSupportingTextColor = Color.Red,
+                    focusedPrefixColor = Color.Blue,
+                    unfocusedPrefixColor = Color.Transparent,
+                    disabledPrefixColor = Color.Transparent,
+                    errorPrefixColor = Color.Red,
+                    focusedSuffixColor = Color.Blue,
+                    unfocusedSuffixColor = Color.Transparent,
+                    disabledSuffixColor = Color.Transparent,
+                    errorSuffixColor = Color.Red
+                )
             )
-        )
+        }
+
+        if (errorIfInvalid!=null && showError) {
+            Text(text = errorIfInvalid, color = Color.Red)
+        }
     }
 }
--- android/app/src/main/java/com/a2/swifting_fitness/screens/compose/LoginForm.kt
+++ android/app/src/main/java/com/a2/swifting_fitness/screens/compose/LoginForm.kt
@@ -2,12 +2,10 @@ package com.a2.swifting_fitness.screens.compose
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.size
-import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -22,22 +20,32 @@ import com.a2.swifting_fitness.R
 @Preview(showSystemUi = true)
 @Composable
 private fun Preview() {
-    Column  (modifier = Modifier.background(color = Color.Black).fillMaxSize()){
+    Column(
+        modifier = Modifier
+            .background(color = Color.Black)
+            .fillMaxSize()
+    ) {
         Spacer(modifier = Modifier.size(50.dp))
         LoginForm { }
     }
 }
+
 @Composable
 fun LoginForm(modifier: Modifier = Modifier, goToHomeScreen: () -> Unit) {
     val (email, setEmail) = remember { mutableStateOf("") }
     val (password, setPassword) = remember { mutableStateOf("") }
-    Column(modifier = modifier){
+    val (showError, setShowError) = remember { mutableStateOf(false) }
+    val emailErrorMessage = if (!email.contains("@")) "Please enter valid email" else null;
+    val passwordErrorMessage =
+        if (password.length < 8) "Password should be at least 8 characters" else null;
+    Column(modifier = modifier) {
         CustomTextField(
             value = email,
             label = "Email",
-
+            errorIfInvalid = emailErrorMessage,
+            showError = showError,
             onValueChange = { setEmail(it) }
-        ){
+        ) {
             Image(
                 painter = painterResource(R.drawable.monotone_email),
                 contentDescription = "Email",
@@ -45,11 +53,13 @@ fun LoginForm(modifier: Modifier = Modifier, goToHomeScreen: () -> Unit) {
         }
         Spacer(Modifier.size(20.dp))
         CustomTextField(
+            errorIfInvalid = passwordErrorMessage,
             value = password,
             label = "Password",
             isPassword = true,
+            showError = showError,
             onValueChange = { setPassword(it) }
-        ){
+        ) {
             Image(
                 painter = painterResource(R.drawable.monotone_lock_password),
                 contentDescription = "Password"
@@ -59,7 +69,11 @@ fun LoginForm(modifier: Modifier = Modifier, goToHomeScreen: () -> Unit) {
         CustomButton(
             title = "Login",
             onClick = {
-
+                if (emailErrorMessage != null || passwordErrorMessage != null) {
+                    setShowError(true);
+                } else {
+                    println("Login request sent")
+                }
             }
         )
 
